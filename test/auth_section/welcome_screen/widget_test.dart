ebec25dfdb5662b872eae529001151f9878af101
tamari gray <49229277+tamari-gray@users.noreply.github.com>
2020-08-03 19:51:01 +1000

    3 welcome screen (#10)
    * tests setup
    
    * working with tests
    
    Co-authored-by: tamari-gray <tamari-gray>

---
+++ test/auth_section/welcome_screen/widget_test.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:niira/main.dart';
+import 'package:niira/screens/create_account.dart';
+import 'package:niira/screens/sign_in.dart';
+
+class MockNavigatorObserver extends Mock implements NavigatorObserver {}
+
+void main() {
+  group('navigation tests', () {
+    NavigatorObserver mockObserver;
+
+    setUp(() {
+      mockObserver = MockNavigatorObserver();
+    });
+
+    testWidgets('navigates to sign in screen', (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(
+        home: WelcomeScreen(),
+        navigatorObservers: [mockObserver],
+      ));
+
+      await tester.tap(find.byKey(WelcomeScreen.navigateToSignInBtn));
+      await tester.pumpAndSettle();
+
+      verify(mockObserver.didPush(any, any));
+
+      expect(find.byType(SignInScreen), findsOneWidget);
+    });
+    testWidgets('navigates to create account screen',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(
+        home: WelcomeScreen(),
+        navigatorObservers: [mockObserver],
+      ));
+
+      await tester.tap(find.byKey(WelcomeScreen.navigateToCreateAccountBtn));
+      await tester.pumpAndSettle();
+
+      verify(mockObserver.didPush(any, any));
+
+      expect(find.byType(CreateAccountScreen), findsOneWidget);
+    });
+  });
+}
