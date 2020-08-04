629551bfaf9e9319cb4b13e190a96bc2ec9f29b5
Nick Meinhold <nick.meinhold@gmail.com>
2020-08-05 00:37:36 +1000

    Create service with relevant models
    - added abstract auth service and implementation with firebase auth
    - added models user data, matches a firebase user
    - added extensions to improve syntax for converting firebase user
    - add a provider at the top pf the tree to provide the new service

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:niira/screens/welcome.dart';
+import 'package:niira/services/auth/auth_service.dart';
+import 'package:niira/services/auth/firebase_auth_service.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,15 +13,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        brightness: Brightness.light,
-        primaryColor: Color.fromRGBO(247, 152, 0, 1),
-        accentColor: Color.fromRGBO(130, 250, 184, 1),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return MultiProvider(
+      providers: [
+        Provider<AuthService>(
+            create: (_) => FirebaseAuthService(FirebaseAuth.instance)),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          brightness: Brightness.light,
+          primaryColor: Color.fromRGBO(247, 152, 0, 1),
+          accentColor: Color.fromRGBO(130, 250, 184, 1),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: WelcomeScreen(),
       ),
-      home: WelcomeScreen(),
     );
   }
 }
---
+++ lib/services/auth/firebase_auth_service.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:niira/models/user_data.dart';
+import 'package:niira/services/auth/auth_service.dart';
+
+import 'package:niira/extensions/firebase_user_extensions.dart';
+
+class FirebaseAuthService implements AuthService {
+  final FirebaseAuth _firebaseAuth;
+
+  FirebaseAuthService(this._firebaseAuth);
+
+  @override
+  Future<String> getCurrentUserId() async {
+    final user = await _firebaseAuth.currentUser();
+    return user.uid;
+  }
+
+  @override
+  Future<UserData> signInWithEmail(String email, String password) async {
+    final authResult = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+    return authResult.user.toData();
+  }
+
+  @override
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+  }
+}
