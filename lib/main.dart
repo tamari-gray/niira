dddcbca5c4cdd15b4ddaf2955c9876b12c41ec18
Nick Meinhold <nick.meinhold@gmail.com>
2020-08-04 23:02:57 +1000

    Add pedantic analysis and strong mode Fixes #18

--- lib/main.dart
+++ lib/main.dart
@@ -88,30 +88,30 @@ class WelcomeScreen extends StatelessWidget {
                         key: navigateToCreateAccountBtn,
                         color: Color.fromRGBO(247, 152, 0, 1),
                         onPressed: () {
-                          Navigator.push(
+                          Navigator.push<dynamic>(
                             context,
-                            MaterialPageRoute(
+                            MaterialPageRoute<dynamic>(
                                 builder: (context) => CreateAccountScreen()),
                           );
                         },
                         child: Text(
-                          "Create account",
+                          'Create account',
                           style: TextStyle(color: Colors.white),
                         ),
                       ),
-                      Text("or"),
+                      Text('or'),
                       OutlineButton(
                         key: navigateToSignInBtn,
                         onPressed: () {
-                          Navigator.push(
+                          Navigator.push<dynamic>(
                             context,
-                            MaterialPageRoute(
+                            MaterialPageRoute<dynamic>(
                                 builder: (context) => SignInScreen()),
                           );
                         },
                         color: Color.fromRGBO(247, 152, 0, 1),
                         child: Text(
-                          "Sign in",
+                          'Sign in',
                           style: TextStyle(color: Colors.white),
                         ),
                       )
