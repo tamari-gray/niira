55e08723179119460ff704941974ccef8bc8ff25
Nick M <nick.meinhold@gmail.com>
2020-08-07 01:59:53 +1000

    Add an integration test
    - just to check the logic as the create account screen is
    not currrently hooked up to the auth service
    - integration tests are currently in an incomplete state but the PR
    was turning into a bit of a beast so leaving completion for another PR

---
+++ test_driver/mocks/services/mock_auth_service.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+
+import 'package:niira/models/user_data.dart';
+import 'package:niira/services/auth/auth_service.dart';
+
+class MockAuthService implements AuthService {
+  final StreamController<UserData> _controller;
+
+  MockAuthService(this._controller);
+
+  @override
+  Future<String> getCurrentUserId() => Future.value('uid');
+
+  @override
+  Stream<UserData> get streamOfAuthState => _controller.stream;
+
+  @override
+  Future<UserData> signInWithEmail(String email, String password) {
+    final data = UserData(
+        uid: 'uid',
+        providerId: 'provideId',
+        displayName: 'name',
+        photoUrl: 'url',
+        email: email,
+        phoneNumber: '123',
+        createdOn: DateTime.now(),
+        lastSignedInOn: DateTime.now(),
+        isAnonymous: false,
+        isEmailVerified: false,
+        providers: []);
+    _controller.add(data);
+    return Future.value(data);
+  }
+
+  @override
+  Future<void> signOut() {
+    _controller.add(null);
+    return Future.value();
+  }
+}
