a1be8bce0e5edb218ffcd4d97085042c2534e7ce
tamari gray <49229277+tamari-gray@users.noreply.github.com>
2020-08-25 12:46:31 +1000

    boundary + game models (#59)
    Co-authored-by: tamari-gray <tamari-gray>

---
+++ lib/models/boundary.dart
@@ -0,0 +1,10 @@
+class Boundary {
+  final int size;
+  final int
+      position; // TODO: position will be an object of latlng coords, update when choose geolocation library
+
+  Boundary(
+    this.size,
+    this.position,
+  );
+}
