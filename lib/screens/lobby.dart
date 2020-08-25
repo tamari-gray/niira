0edffc305ec2772d7c4df94400766b8f79e77e7b
Nick Meinhold <nick.meinhold@gmail.com>
2020-08-25 14:37:50 +1000

    51 add showConfirmationDialog to nav service Fixes #51

--- lib/screens/lobby.dart
+++ lib/screens/lobby.dart
@@ -1,42 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:niira/services/auth/auth_service.dart';
+import 'package:niira/services/navigation_service.dart';
 import 'package:provider/provider.dart';
 
 class LobbyScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Future<void> _confirmSignOutDialog() async {
-      return showDialog<void>(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text('Are you sure?'),
-            actions: <Widget>[
-              OutlineButton(
-                color: Theme.of(context).primaryColor,
-                highlightedBorderColor: Theme.of(context).primaryColor,
-                textColor: Theme.of(context).primaryColor,
-                key: Key('confirmSignOutBtn'),
-                child: Text('Sign out'),
-                onPressed: () async {
-                  Navigator.of(context).pop();
-                  await context.read<AuthService>().signOut();
-                },
-              ),
-              FlatButton(
-                color: Colors.white,
-                textColor: Theme.of(context).primaryColor,
-                child: Text('Return'),
-                onPressed: () async {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        },
-      );
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Lobby'),
@@ -44,7 +13,16 @@ class LobbyScreen extends StatelessWidget {
           FlatButton(
             key: Key('signOutBtn'),
             onPressed: () {
-              _confirmSignOutDialog();
+              // create a function to call on confirmation
+              final signOut = () async {
+                Navigator.of(context).pop();
+                await context.read<AuthService>().signOut();
+              };
+
+              context.read<NavigationService>().showConfirmationDialog(
+                  onConfirmed: signOut,
+                  confirmText: 'Sign Out',
+                  cancelText: 'Return');
             },
             child: Text('log out'),
           )
