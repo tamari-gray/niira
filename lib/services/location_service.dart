00bddd3ebebea7e427bf1685346c7d07cb56f1d6
tamari-gray <tamari-gray>
2020-08-27 17:19:40 +1000

    started service

---
+++ lib/services/location_service.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+
+import 'package:geolocator/geolocator.dart';
+import 'package:niira/models/user_location.dart';
+
+class LocationService {
+  final Geolocator _geolocator;
+  LocationService(this._geolocator);
+
+  final _controller = StreamController<UserLocation>();
+
+  // check if location services are enabled
+  Future<GeolocationStatus> checkForLocationPermission() async {
+    return await _geolocator.checkGeolocationPermissionStatus();
+  }
+
+  // get users currentLocation
+  Future<Position> getUsersCurrentLocation() async {
+    return await _geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+  }
+
+  //
+
+  // listen to users location
+  void listenToUsersLocation() {
+    var geolocator = _geolocator;
+    var locationOptions = LocationOptions(
+      accuracy: LocationAccuracy.best,
+      distanceFilter: 0,
+    );
+
+    StreamSubscription positionStream = geolocator
+        .getPositionStream(locationOptions)
+        .listen((Position position) {
+      position != null
+          ? _controller.add(
+              UserLocation(
+                latitude: position.latitude,
+                longitude: position.longitude,
+              ),
+            )
+          : print('no location');
+    });
+  }
+}
