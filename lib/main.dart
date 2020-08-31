579b37bf6b8d7494bea4290737fb15cedbf01e9a
Nick Meinhold <nick.meinhold@gmail.com>
2020-08-31 19:20:42 +1000

    Add a map widget and test
    - added permission_handler package for requesting permissions
    - added steps in gradle to retrieve key from local.properties
    - added meta-data tag to manifest file to provide API key to maps plugin
    - added the demo map widget page to test key is working

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:niira/screens/lobby.dart';
-import 'package:niira/screens/welcome.dart';
+import 'package:niira/map_page.dart';
 import 'package:niira/services/auth/auth_service.dart';
 import 'package:niira/services/auth/firebase_auth_service.dart';
 import 'package:niira/services/database/database_service.dart';
 import 'package:niira/services/database/firestore_service.dart';
 import 'package:niira/services/navigation_service.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
@@ -30,7 +30,7 @@ void main() async {
       navigationService));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   final AuthService _authService;
   final DatabaseService _databaseService;
   final NavigationService _navigationService;
@@ -40,16 +40,33 @@ class MyApp extends StatelessWidget {
       this._navigationService);
 
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    _requestPermissions();
+    super.initState();
+  }
+
+  void _requestPermissions() async {
+    if (await Permission.location.request().isGranted) {
+      // Either the permission was already granted before or the user just granted it.
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        Provider<AuthService>.value(value: _authService),
-        Provider<DatabaseService>.value(value: _databaseService),
-        Provider<NavigationService>.value(value: _navigationService),
+        Provider<AuthService>.value(value: widget._authService),
+        Provider<DatabaseService>.value(value: widget._databaseService),
+        Provider<NavigationService>.value(value: widget._navigationService),
       ],
       child: MaterialApp(
           title: 'Flutter Demo',
-          navigatorKey: _navigatorKey,
+          navigatorKey: widget._navigatorKey,
           theme: ThemeData(
             brightness: Brightness.light,
             primaryColor: Color.fromRGBO(247, 152, 0, 1),
@@ -57,10 +74,11 @@ class MyApp extends StatelessWidget {
             visualDensity: VisualDensity.adaptivePlatformDensity,
           ),
           home: StreamBuilder(
-            stream: _authService.streamOfAuthState,
+            stream: widget._authService.streamOfAuthState,
             builder: (context, snapshot) {
+              return MapPage();
               // TODO: check for snapshot error and send to navigation manager for display
-              return (snapshot.data == null) ? WelcomeScreen() : LobbyScreen();
+              // return (snapshot.data == null) ? WelcomeScreen() : LobbyScreen();
             },
           )),
     );
