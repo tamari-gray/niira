bc231e23adef938714b278e9dd7c27514039c1b8
tamari-gray <tamari-gray>
2020-09-02 16:21:42 +1000

    started test

--- test/app_section/app_widget_test.dart
+++ test/app_section/app_widget_test.dart
@@ -18,6 +18,49 @@ void main() {
     Firebase.delegatePackingProperty = MockFirebasePlatform();
   });
   group('MyApp ', () {
+    testWidgets(
+        'show loading icon until all services are initialised', //TODO: finish test
+        (WidgetTester tester) async {
+      // create a controller that the mock auth servive will hold
+      final controller = StreamController<UserData>();
+      final mockUserData = MockUser().userData;
+      final mockNavService = MockNavService();
+      final mockAuthService = MockAuthService(
+        controller: controller,
+        mockUserData: mockUserData,
+        mockNavService: mockNavService,
+        successfulAuth: true,
+      );
+      final mockDatabaseStreamController = StreamController<List<Game>>();
+      final mockDatabaseService =
+          MockDatabaseService(controller: mockDatabaseStreamController);
+
+      // create the widget under test
+      await tester.pumpWidget(MyApp(
+        authService: mockAuthService,
+        navigatorKey: GlobalKey<NavigatorState>(),
+        databaseService: mockDatabaseService,
+        navigation: mockNavService,
+      ));
+
+      // update auth stream to have no signed in user
+      controller.add(null);
+      await tester.pumpAndSettle();
+
+      // check the welcome screen is present
+      expect(find.byKey(Key('navigateToCreateAccount')), findsOneWidget);
+
+      // get a user data object and make the service emit it
+      final userData = await mockAuthService.signInWithEmail('a', 'b');
+      controller.add(userData);
+
+      await tester.pump();
+
+      // check the welcome screen is no longer present
+      expect(find.byKey(Key('navigateToCreateAccount')), findsNothing);
+      // check that the lobby screen is present
+      expect(find.text('Lobby'), findsOneWidget);
+    });
     testWidgets('navigates to correct route based on auth state',
         (WidgetTester tester) async {
       // create a controller that the mock auth servive will hold
