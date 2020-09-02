01699d598d0d817cc6b23c87e71166280f5b9a0e
tamari-gray <tamari-gray>
2020-09-02 13:46:58 +1000

    initialise services inside stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:niira/loading.dart';
 import 'package:niira/screens/lobby.dart';
 import 'package:niira/screens/welcome.dart';
 import 'package:niira/services/auth/auth_service.dart';
@@ -13,42 +14,48 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  // init firebase
-  try {
-    await Firebase.initializeApp();
-  } catch (e) {
-    // show user an error message
-  }
-
-  // init services
-  final navigationService = NavigationService();
-  final authService =
-      FirebaseAuthService(FirebaseAuth.instance, navigationService);
-  final databaseService = FirestoreService(FirebaseFirestore.instance);
-
-  runApp(MyApp(authService, navigationService.navigatorKey, databaseService,
-      navigationService));
+  WidgetsFlutterBinding.ensureInitialized(); //TODO: delete this line??
+  runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  final AuthService _authService;
-  final DatabaseService _databaseService;
-  final NavigationService _navigationService;
-  final GlobalKey<NavigatorState> _navigatorKey;
-
-  MyApp(this._authService, this._navigatorKey, this._databaseService,
-      this._navigationService);
-
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  bool _loadingServices;
+  AuthService _authService;
+  DatabaseService _databaseService;
+  NavigationService _navigation;
+  GlobalKey<NavigatorState> _navigatorKey;
   @override
   void initState() {
     _requestPermissions();
     super.initState();
+    _loadingServices = true;
+    initServices();
+  }
+
+  void initServices() async {
+    // init firebase
+    try {
+      await Firebase.initializeApp();
+    } catch (e) {
+      // show user an error message
+    }
+
+    // init services
+    final navigation = NavigationService();
+    final authService = FirebaseAuthService(FirebaseAuth.instance, navigation);
+    final databaseService = FirestoreService(FirebaseFirestore.instance);
+
+    setState(() {
+      _navigation = navigation;
+      _authService = authService;
+      _databaseService = databaseService;
+      _loadingServices = false;
+    });
   }
 
   void _requestPermissions() async {
@@ -59,28 +66,33 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider<AuthService>.value(value: widget._authService),
-        Provider<DatabaseService>.value(value: widget._databaseService),
-        Provider<NavigationService>.value(value: widget._navigationService),
-      ],
-      child: MaterialApp(
-          title: 'Flutter Demo',
-          navigatorKey: widget._navigatorKey,
-          theme: ThemeData(
-            brightness: Brightness.light,
-            primaryColor: Color.fromRGBO(247, 152, 0, 1),
-            accentColor: Color.fromRGBO(130, 250, 184, 1),
-            visualDensity: VisualDensity.adaptivePlatformDensity,
-          ),
-          home: StreamBuilder(
-            stream: widget._authService.streamOfAuthState,
-            builder: (context, snapshot) {
-              // TODO: check for snapshot error and send to navigation manager for display
-              return (snapshot.data == null) ? WelcomeScreen() : LobbyScreen();
-            },
-          )),
-    );
+    return _loadingServices
+        ? Loading()
+        : MultiProvider(
+            providers: [
+              Provider<AuthService>.value(value: _authService),
+              Provider<DatabaseService>.value(value: _databaseService),
+              Provider<NavigationService>.value(value: _navigation),
+            ],
+            child: MaterialApp(
+              title: 'Flutter Demo',
+              navigatorKey: _navigatorKey,
+              theme: ThemeData(
+                brightness: Brightness.light,
+                primaryColor: Color.fromRGBO(247, 152, 0, 1),
+                accentColor: Color.fromRGBO(130, 250, 184, 1),
+                visualDensity: VisualDensity.adaptivePlatformDensity,
+              ),
+              home: StreamBuilder(
+                stream: _authService.streamOfAuthState,
+                builder: (context, snapshot) {
+                  // TODO: check for snapshot error and send to navigation manager for display
+                  return (snapshot.data == null)
+                      ? WelcomeScreen()
+                      : LobbyScreen();
+                },
+              ),
+            ),
+          );
   }
 }
