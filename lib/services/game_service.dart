d3884f3c996d1682fa9229d68d03ec482d5e536c
tamari-gray <tamari-gray>
2020-09-06 14:44:41 +1000

    pass selected game to provider state

--- lib/services/game_service.dart
+++ lib/services/game_service.dart
@@ -3,4 +3,6 @@ import 'package:niira/models/game.dart';
 class GameService {
   Game currentGame;
   GameService({this.currentGame});
+
+  void setCurrentGame(Game _selectedGame) => currentGame = _selectedGame;
 }
