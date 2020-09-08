ea60f0475416fff052d716c4a86539d6d0059dc8
tamari-gray <tamari-gray>
2020-09-09 01:37:47 +1000

    changed from final to consts

--- lib/navigation/route_names.dart
+++ lib/navigation/route_names.dart
@@ -1,6 +1,6 @@
-final String waiting_for_game_to_start = '/waiting_for_game_to_start';
-final String create_account = '/create_account';
-final String sign_in = '/sign_in';
-final String new_game1 = '/new_game1';
-final String new_game2 = '/new_game2';
-final String input_password = '/input_password';
+const String waiting_for_game_to_start = '/waiting_for_game_to_start';
+const String create_account = '/create_account';
+const String sign_in = '/sign_in';
+const String new_game1 = '/new_game1';
+const String new_game2 = '/new_game2';
+const String input_password = '/input_password';
