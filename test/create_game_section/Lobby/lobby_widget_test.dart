47293a3a4deb6a484f211d7ea4980a770b0ab259
tamari-gray <tamari-gray>
2020-09-09 14:04:44 +1000

    started testing

--- test/create_game_section/Lobby/lobby_widget_test.dart
+++ test/create_game_section/Lobby/lobby_widget_test.dart
@@ -7,15 +7,59 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:niira/main.dart';
 import 'package:niira/models/game.dart';
 import 'package:niira/models/user_data.dart';
+import 'package:niira/screens/lobby/lobby.dart';
+import 'package:niira/screens/lobby/list_of_created_games.dart';
+import 'package:niira/services/auth/auth_service.dart';
 import 'package:niira/navigation/navigation.dart';
+import 'package:niira/services/database/database_service.dart';
+import 'package:niira/services/location_service.dart';
+import 'package:provider/provider.dart';
+
 import '../../mocks/mock_user_data.dart';
 import '../../mocks/services/mock_auth_service.dart';
 import '../../mocks/services/mock_database_service.dart';
+import '../../mocks/services/mock_location_service.dart';
 import '../../mocks/services/mock_firebase_platform.dart';
 
 void main() {
   setUp(() {
     Firebase.delegatePackingProperty = MockFirebasePlatform();
+  });
+  testWidgets(
+      'show loading icon until have users location, then show list of created games ',
+      (WidgetTester tester) async {
+    // init services
+    final controller = StreamController<UserData>();
+    final navigation = Navigation();
+    final mockAuthService = MockAuthService(controller: controller);
+    final mockLocationService = MockLocationService();
+    final mockDatabseController = StreamController<List<Game>>();
+    final mockDatabaseService =
+        MockDatabaseService(controller: mockDatabseController);
+
+    // create the widget under test
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          Provider<AuthService>.value(value: mockAuthService),
+          Provider<Navigation>.value(value: navigation),
+          Provider<LocationService>.value(value: mockLocationService),
+          Provider<DatabaseService>.value(value: mockDatabaseService)
+        ],
+        child: MaterialApp(
+          home: LobbyScreen(),
+        ),
+      ),
+    );
+
+    // show loading icon
+    expect(find.byKey(Key('loading_indicator')), findsOneWidget);
+
+    // get users location
+    await tester.pumpAndSettle();
+
+    // show list of created games
+    expect(find.byType(ListOfCreatedGames), findsOneWidget);
   });
   testWidgets(
       'shows loading icon + redirects to welcome screen on successfull logout',
@@ -27,12 +71,14 @@ void main() {
     final createdGamesStreamContoller = StreamController<List<Game>>();
     final mockDBService =
         MockDatabaseService(controller: createdGamesStreamContoller);
+    final mockLocationService = MockLocationService();
     final mockUserData = MockUser().userData;
 
     // create the widget under test
     await tester.pumpWidget(MyApp(
       authService: mockAuthService,
       databaseService: mockDBService,
+      locationService: mockLocationService,
       navigation: navigation,
     ));
 
@@ -52,7 +98,6 @@ void main() {
 
     // check the lobby screen is no longer present
     expect(find.text('Lobby'), findsNothing);
-
     // check that the welcome screen is present
     expect(find.byKey(Key('navigateToCreateAccount')), findsOneWidget);
   });
