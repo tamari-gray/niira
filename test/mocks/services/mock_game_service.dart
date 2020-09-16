e79bb989a5e15b8fe436d31c5999aef13b2839c7
tamari-gray <tamari-gray>
2020-09-16 22:04:41 +1000

    deleted "extends fake" fix

--- test/mocks/services/mock_game_service.dart
+++ test/mocks/services/mock_game_service.dart
@@ -1,8 +1,9 @@
+import 'package:mockito/mockito.dart';
 import 'package:niira/models/game.dart';
 import 'package:niira/models/view_models/create_game1.dart';
 import 'package:niira/services/game_service.dart';
 
-class FakeGameService implements GameService {
+class FakeGameService extends Fake implements GameService {
   @override
   final createGameViewModel1 = CreateGameViewModel1();
   @override
