3d05c8e68ff7abd0131705df0fdd514a759d7296
tamari-gray <tamari-gray>
2020-09-21 22:15:55 +1000

    lobby reinstantiated!

---
+++ lib/screens/lobby/lobby.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:niira/loading.dart';
+import 'package:niira/models/location.dart';
+import 'package:niira/screens/create_game1/create_game_screen1.dart';
+import 'package:niira/screens/Lobby/list_of_created_games.dart';
+import 'package:niira/services/auth/auth_service.dart';
+import 'package:niira/navigation/navigation.dart';
+import 'package:niira/services/location_service.dart';
+import 'package:provider/provider.dart';
+
+class LobbyScreen extends StatelessWidget {
+  // TODO: decide on what happens when user refuses location
+  static const routeName = '/lobby';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lobby'),
+        actions: [
+          FlatButton(
+            key: Key('signOutBtn'),
+            onPressed: () {
+              // create a function to sign out user on dialog confirmation
+              final signOut = () async {
+                context.read<Navigation>().pop();
+                await context.read<AuthService>().signOut();
+              };
+
+              context.read<Navigation>().showConfirmationDialog(
+                    onConfirmed: signOut,
+                    confirmText: 'Sign Out',
+                    cancelText: 'Return',
+                  );
+            },
+            child: Text('log out'),
+          )
+        ],
+      ),
+      body: FutureBuilder<Location>(
+          future: context.watch<LocationService>().getUsersCurrentLocation(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData == false) {
+              return Loading();
+            } else {
+              return ListOfCreatedGames(snapshot.data);
+            }
+          }),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () =>
+            context.read<Navigation>().navigateTo(CreateGameScreen1.routeName),
+        label: Text('New Game'),
+        icon: Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ test/create_game_section/lobby/list_of_created_games_widget_test.dart
@@ -0,0 +1,83 @@
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:niira/models/game.dart';
+import 'package:niira/models/location.dart';
+import 'package:niira/screens/lobby/list_of_created_games.dart';
+import 'package:niira/screens/lobby/created_game_tile.dart';
+import 'package:niira/services/database/database_service.dart';
+import 'package:niira/navigation/navigation.dart';
+import 'package:niira/services/location_service.dart';
+import 'package:provider/provider.dart';
+
+import '../../mocks/data/mock_games.dart';
+import '../../mocks/services/mock_database_service.dart';
+import '../../mocks/services/mock_location_service.dart';
+import '../../mocks/services/mock_firebase_platform.dart';
+
+void main() {
+  setUp(() {
+    Firebase.delegatePackingProperty = MockFirebasePlatform();
+  });
+  testWidgets(
+      'show list of games in order of distance from user (nearest to furtherest)',
+      (WidgetTester tester) async {
+    // init services
+    final navigation = Navigation();
+    final mockLocationService = FakeLocationService();
+    final mockDatabseController = StreamController<List<Game>>();
+    final mockCreatedGames = gamesInorderOfDistance;
+    final mockDatabaseService =
+        MockDatabaseService(controller: mockDatabseController);
+    final mockUserLocation =
+        Location(latitude: -37.865351, longitude: 144.989012);
+
+    // add mockGames
+    mockDatabseController.add(mockCreatedGames);
+    await tester.pumpAndSettle();
+
+    // create the widget under test
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          Provider<Navigation>.value(value: navigation),
+          Provider<LocationService>.value(value: mockLocationService),
+          Provider<DatabaseService>.value(value: mockDatabaseService)
+        ],
+        child: MaterialApp(
+          home: ListOfCreatedGames(mockUserLocation),
+        ),
+      ),
+    );
+
+    // find empty screen while waiting for stream of created games
+    expect(
+        find.byKey(Key('list_of_created_games_empty_screen')), findsOneWidget);
+
+    // ol reliable
+    await tester.pumpAndSettle();
+
+    // show correct widgets
+    expect(find.byKey(Key('list_of_created_games_empty_screen')), findsNothing);
+    expect(find.byType(GameTile), findsNWidgets(3));
+
+    /// show list of created games inorder of distance from user.
+    /// when building game tile, we are giving it an 'index' property,
+    /// to help test that they are rendered in correct order
+    expect(
+      find.byKey(Key('created_game_tile_${mockCreatedGames[0].id}_index:${0}')),
+      findsOneWidget,
+    );
+    expect(
+      find.byKey(Key('created_game_tile_${mockCreatedGames[1].id}_index:${1}')),
+      findsOneWidget,
+    );
+    expect(
+      find.byKey(Key('created_game_tile_${mockCreatedGames[2].id}_index:${2}')),
+      findsOneWidget,
+    );
+  });
+}
