dbc465f0c610e06805643e8cf8e13033321df28e
tamari-gray <tamari-gray>
2020-10-03 12:42:13 +1000

    updated validators to clearer errror messages

--- lib/utilities/validators.dart
+++ lib/utilities/validators.dart
@@ -1,11 +1,11 @@
 final gameName = (String value) => value.isEmpty
-    ? 'Please enter at least 1 character'
+    ? 'Please enter game name'
     : value.length > 8
         ? 'Game names must be less then 8 characters.'
         : null;
 
 final gamePassword = (String value) => value.isEmpty
-    ? 'Please enter at least 1 character'
-    : value.length > 8
-        ? 'Passwords must be less then 8 characters.'
+    ? 'Please enter game password'
+    : value.length > 15
+        ? 'Passwords must be less then 15 characters.'
         : null;
