9c07d8a56d9399628e8c8f486933538bead04778
tamari-gray <tamari-gray>
2020-10-03 20:49:23 +1000

    tested and ready 🙈

--- test/app_section/app_widget_test.dart
+++ test/app_section/app_widget_test.dart
@@ -19,7 +19,8 @@ void main() {
     Firebase.delegatePackingProperty = MockFirebasePlatform();
   });
   group('MyApp ', () {
-    testWidgets('show loading icon until all services are initialised',
+    testWidgets(
+        'show loading icon until all services + firebase are initialised',
         (WidgetTester tester) async {
       // create mock services
       final controller = StreamController<UserData>();
@@ -36,16 +37,25 @@ void main() {
       final mockDatabaseService =
           MockDatabaseService(controller: mockDatabaseStreamController);
 
+      // create a fake firebase wrapper with a supplied completer
+      final firebaseCompleter = Completer<FirebaseApp>();
+      final firebase = FakeFirebaseWrapper(completer: firebaseCompleter);
+
       // create the widget under test
       await tester.pumpWidget(MyApp(
         authService: mockAuthService,
         databaseService: mockDatabaseService,
         navigation: fakeNavigation,
+        firebase: firebase,
       ));
 
       // check for loading widget
       expect(find.byKey(Key('loading_indicator')), findsOneWidget);
+
+      // init firebase
+      firebaseCompleter.complete();
       await tester.pumpAndSettle();
+
       expect(find.byKey(Key('loading_indicator')), findsNothing);
     });
     testWidgets('navigates to correct route based on auth state',
@@ -97,6 +107,43 @@ void main() {
       expect(find.byKey(Key('navigateToCreateAccount')), findsNothing);
       // check that the lobby screen is present
       expect(find.text('Lobby'), findsOneWidget);
+    });
+    testWidgets('show error message when error initialising firebase',
+        (WidgetTester tester) async {
+      // create a controller that the mock auth servive will hold
+      final controller = StreamController<UserData>();
+      final mockUserData = MockUser().userData;
+      final fakeNavigation = FakeNavigation();
+      final mockAuthService = MockAuthService(
+        controller: controller,
+        mockUserData: mockUserData,
+        fakeNavigation: fakeNavigation,
+        successfulAuth: true,
+      );
+      final mockDatabaseStreamController = StreamController<List<Game>>();
+      final mockDatabaseService =
+          MockDatabaseService(controller: mockDatabaseStreamController);
+
+      // create a fake firebase wrapper with a supplied completer
+      final firebaseCompleter = Completer<FirebaseApp>();
+      final firebase = FakeFirebaseWrapper(completer: firebaseCompleter);
+
+      // create the widget under test
+      await tester.pumpWidget(MyApp(
+        authService: mockAuthService,
+        databaseService: mockDatabaseService,
+        navigation: fakeNavigation,
+        firebase: firebase,
+      ));
+
+      // init firebase error
+      final errorMsg = 'error initialising firebase';
+      firebaseCompleter.completeError(
+          errorMsg, StackTrace.fromString('stacktrace yee'));
+      await tester.pumpAndSettle();
+
+      // checks that error message is shown
+      expect(find.text(errorMsg), findsOneWidget);
     });
   });
 }
