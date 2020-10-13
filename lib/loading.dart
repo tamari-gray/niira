6ced32bb7beb7d1109c4b9333c257f3005a41365
tamari gray <49229277+tamari-gray@users.noreply.github.com>
2020-10-13 21:23:34 +1100

    show text in loading indicator to give user more information (#133)
    Fixes #131
    
    Co-authored-by: tamari-gray <tamari-gray>

--- lib/loading.dart
+++ lib/loading.dart
@@ -2,18 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatelessWidget {
-  const Loading({Key key}) : super(key: key);
+  final String message;
+  Loading({Key key, this.message}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: Center(
-        child: SpinKitPulse(
-          key: Key('loading_indicator'),
-          color: Theme.of(context).primaryColor,
-          size: 100.0,
-        ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SpinKitPulse(
+            key: Key('loading_indicator'),
+            color: Theme.of(context).primaryColor,
+            size: 100.0,
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(25, 50, 25, 0),
+            child: Directionality(
+              textDirection: TextDirection.ltr,
+              child: Text(
+                '$message',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                    color: Theme.of(context).primaryColor),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
