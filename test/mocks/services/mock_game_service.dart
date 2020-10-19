193951358fbe73f2ba68ddb8f4e6e7be5de40db8
tamari-gray <tamari-gray>
2020-10-19 19:17:46 +1100

    started testing, leaving for another issue

--- test/mocks/services/mock_game_service.dart
+++ test/mocks/services/mock_game_service.dart
@@ -1,13 +1,19 @@
 import 'package:mockito/mockito.dart';
 import 'package:niira/models/game.dart';
 import 'package:niira/models/view_models/create_game1.dart';
+import 'package:niira/models/view_models/create_game2.dart';
 import 'package:niira/services/game_service.dart';
 
 class FakeGameService extends Fake implements GameService {
   @override
+  Game currentGame;
+  @override
   final createGameViewModel1 = CreateGameViewModel1();
   @override
-  Game currentGame;
+  final createGameViewModel2 = CreateGameViewModel2(defaultBoundarySize: 150);
+
+  @override
+  bool get hasListeners => false;
 }
 
 class MockGameService extends Mock implements GameService {}
