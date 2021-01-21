20c5d320f796fe2982420647a07862b92ac9b62a
tamari-gray <tamari-gray>
2021-01-21 14:14:41 +1100

    fixes finished game screen #165

--- lib/screens/joined_game_screens/finished_game_screen.dart
+++ lib/screens/joined_game_screens/finished_game_screen.dart
@@ -14,32 +14,38 @@ class FinishedGameScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (playerDoc.isTagger) {
-      if (playerDoc.hasQuit) {
-        // show "you lose"
-
-      } else if (!playerDoc.hasQuit) {
-        // show "you win! you tagged everybody!"
-      }
-    } else if (!playerDoc.isTagger) {
-      // calculate position
-
-    } else {
-      print('huston we have a problem. finished game screen');
-    }
     return Scaffold(
       appBar: AppBar(
-        title: Text('${game.name}'),
+        title: Text('Niira'),
       ),
       body: Container(
-        child: Center(
-          child: RaisedButton(
-            child: Text('play again'),
-            onPressed: () async {
-              final userId = await context.read<AuthService>().currentUserId;
-              await context.read<DatabaseService>().leaveGame(game.id, userId);
-            },
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 15),
+              child: Center(
+                child: Text(
+                  playerDoc.hasQuit || playerDoc.hasBeenTagged
+                      ? 'You lose'
+                      : 'You win!',
+                  style: TextStyle(fontSize: 25),
+                ),
+              ),
+            ),
+            Center(
+              child: RaisedButton(
+                child: Text('Home'),
+                onPressed: () async {
+                  final userId =
+                      await context.read<AuthService>().currentUserId;
+                  await context
+                      .read<DatabaseService>()
+                      .leaveGame(game.id, userId);
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
